e7a743ac5d472017ca5353238d5badb9a5299a3f
freewinkin <freewink.in@gmail.com>
2026-07-24 15:51:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import LandingPage from "./LandingPage";
-import XvoraOnboarding from "./XvoraOnboarding";
+import XvoraOnboarding, { StartupBriefInput } from "./XvoraOnboarding";
 
 export default function App() {
   const [activeView, setActiveView] = useState<"landing" | "onboarding">("landing");
+
+  const handleComplete = (data: StartupBriefInput) => {
+    // TODO: hand this off to wherever the Startup Brief gets generated/displayed.
+    console.log("Startup Brief inputs:", data);
+    setActiveView("landing");
+  };
 
   return (
     <div>
       {activeView === "landing" ? (
         <LandingPage onStart={() => setActiveView("onboarding")} />
       ) : (
-        <XvoraOnboarding onBack={() => setActiveView("landing")} />
+        <XvoraOnboarding
+          onBack={() => setActiveView("landing")}
+          onComplete={handleComplete}
+        />
       )}
     </div>
   );
